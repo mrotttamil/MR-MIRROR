6bb9e08afa0259a74a15c78426883fa8acd3ea95
Thor⚡ <82438870+Thor-Ragnarok@users.noreply.github.com>
2021-04-30 21:02:45 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,7 +44,7 @@ def stats(update, context):
             f'<b>CPU:</b> {cpuUsage}%\n' \
             f'<b>RAM:</b> {memory}%\n' \
             f'<b>Disk:</b> {disk}%'
-    update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", stats, parse_mode=ParseMode.HTML)
+    update.effective_message.reply_photo("https://telegra.ph/file/5a5cbe60c716f93a65e23.jpg", stats, parse_mode=ParseMode.HTML)
 
 
 @run_async
@@ -53,7 +53,7 @@ def start(update, context):
 This bot can mirror all your links to Google drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", start_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/5a5cbe60c716f93a65e23.jpg", start_string, parse_mode=ParseMode.MARKDOWN)
 
 
 @run_async
@@ -67,7 +67,7 @@ def chat_list(update, context):
 def repo(update, context):
     bot.send_message(update.message.chat_id,
     reply_to_message_id=update.message.message_id,
-    text="Repo: https://github.com/breakdowns/slam-mirrorbot\nGroup: https://t.me/SlamMirrorSupport", disable_web_page_preview=True)
+    text="Repo: https://github.com/Thor-Ragnarok/slam-mirrorbot\nGroup: https://t.me/TN57_BotZSupport", disable_web_page_preview=True)
 
 
 @run_async
